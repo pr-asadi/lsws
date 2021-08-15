a4ff784ab9fee6b43a8eb9bc6a93de62841d852f
Pouria Asadi <84969067+pr-asadi@users.noreply.github.com>
2021-08-15 19:52:45 +0430

    Updated
    Date added to selected wallpaper: lsws
    
    Updated: setup.sh uninstall.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,78 @@
 #!/bin/sh
-USERNAME=`logname`
-UID_TEMP=`id -u $USER`
-if [ "$UID_TEMP" == 0 ];then
+# vim: foldmethod=marker tabstop=4 softtabstop=4 shiftwidth=4 syntax=sh filetype=sh
+
+# Global Variables {{{
+
+USERNAME="$(logname)"
+UID_TEMP="$(id -u)"
+GREEN="\033[0;32m"
+RED="\033[0;31m"
+RESET="\033[0m"
+
+# }}}
+
+# Warn Function {{{
+
+warn() {
+	local YELLOW="\033[0;33m"
+	local RESET="\033[0m"
+	printf "${YELLOW}%s${RESET}\n" "$*" >&2
+	unset YELLOW RESET
+
+}
+
+# }}}
+
+if [ "${UID_TEMP}" -eq 0 ];then
+	printf "${GREEN}"
 	mkdir -p -v "/home/$USERNAME/.lsws/" "/home/$USERNAME/.lsws/config/" "/home/$USERNAME/.lsws/logs/"
+	printf "${RESET}"
+
 	if [ ! -e /usr/local/man/man1/ ];then
+		warn "/usr/local/man/man1/ directory does not exists." "Creating..."
 		mkdir -v /usr/local/man/man1/
 	fi
-	mv -v ./lsws.1.gz /usr/local/man/man1/
-	mv -v ./lsws.1 /usr/local/man/man1/
-	if [ -d "/home/$USERNAME/.lsws/" ] && [ -d "/home/$USERNAME/.lsws/config/" ] && [ -d "/home/$USERNAME/.lsws/logs/" ];then
+
+	if [ -f "./lsws.1.gz" ]; then
+		printf "${GREEN}"
+		mv -v ./lsws.1.gz /usr/local/man/man1/
+		printf "${RESET}"
+
+	else
+		warn "./lsws.1.gz does not exists."
+
+	fi
+
+	if [ -f "./lsws.1" ]; then
+		printf "${GREEN}"
+		mv -v ./lsws.1 /usr/local/man/man1/
+		printf "${RESET}"
+
+	else
+		warn "./lsws.1 does not exists."
+
+	fi
+
+	if [ -d "/home/$USERNAME/.lsws/" -a -d "/home/$USERNAME/.lsws/config/" \
+			-a -d "/home/$USERNAME/.lsws/logs/" ]; then
+		if [ -f "./lsws.defaults" ]; then
+			printf "${GREEN}"
+			mv -v ./lsws.defaults "/home/${USERNAME}/.lsws/config/"
+			printf "${RESET}"
+
+		else
+			warn "./lsws.defaults does not exists."
+
+		fi
+
+		printf "${GREEN}"
 		touch ./Seasonal.defaults
-		mv -v ./lsws.defaults "/home/${USERNAME}/.lsws/config/"
 		mv -v ./Seasonal.defaults "/home/${USERNAME}/.lsws/config/"
+		printf "${RESET}"
 		chown -R $USERNAME /home/${USERNAME}/.lsws
 	fi
 else
-	printf "\033[0;31m This script should run with root privilege\033[0m\n"
+	printf "${RED}This script should run with root privilege${RESET}\n"
 fi
+
+unset USERNAME UID_TEMP GREEN RED RESET
--- uninstall.sh
+++ uninstall.sh
@@ -1,19 +1,82 @@
 #!/bin/sh
+# vim: foldmethod=marker tabstop=4 softtabstop=4 shiftwidth=4 syntax=sh filetype=sh
+
+# Global Variables {{{
+
 USERNAME=$(logname)
-UID_TEMP=$(id -u "${USERNAME}")
+UID_TEMP=$(id -u)
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+RESET="\033[0m"
+	
+# }}}
+
+# Warn Function {{{
+warn() {
+	local FILE_NAME="$0"
+	local FILE_NAME="$(basename ${FILE_NAME})"
+	local YELLOW="\033[0;33m"
+	local RESET="\033[0m"
+	printf "${FILE_NAME}: ${YELLOW}%s${RESET}\n" "$*" >&2
+	unset YELLOW RESET FILE_NAME
+
+}
+
+# }}}
+
+# Log Function {{{
+
+log() {
+	local FILE_NAME="$0"
+	local FILE_NAME="$(basename ${FILE_NAME})"
+	local GREEN="\033[0;32m"
+	local RESET="\033[0m"
+	printf "${FILE_NAME}: ${GREEN}%s${RESET}\n" "$*"
+	unset GREEN RESET FILE_NAME
+
+}
+
+# }}}
 
 if [ "${UID_TEMP}" -eq 0 ]; then
-	rm -v /usr/local/man/man1/lsws.1 /usr/local/man/man1/lsws.1.gz
+
+	if [ -d "/usr/local/man/man1/" ]; then
+
+		if [ -f "/usr/local/man/man1/lsws.1" ]; then
+			printf "${GREEN}"
+			rm -v /usr/local/man/man1/lsws.1
+			printf "${RESET}"
+		else
+			warn "/usr/local/man/man1/lsws.1 does not exists."
+		fi
+
+		if [ -f "/usr/local/man/man1/lsws.1.gz" ]; then
+			printf "${GREEN}"
+			rm -v /usr/local/man/man1/lsws.1.gz
+			printf "${RESET}"
+		else
+			warn "/usr/local/man/man1/lsws.1.gz does not exists."
+		fi
+
+	fi
 else
-	printf "\033[0;31mRun with sudo\033[0m\n"
+	printf "${RED}Run with sudo/doas${RESET}\n"
 	exit 1
 fi
 
-if [ -d "/home/${USERNAME}/.lsws" ]; then
+if [ -d "/home/${USERNAME}/.lsws/" ]; then
 	rm -rf "/home/${USERNAME}/.lsws/"
+	log "/home/${USERNAME}/.lsws/ removed."
+else
+	warn "/home/${USERNAME}/.lsws/ does not exists."
 
 fi
 
 if [ -d "/home/${USERNAME}/lsws/" ]; then
 	rm -rf "/home/${USERNAME}/lsws/"
+	log "/home/${USERNAME}/lsws/ removed."
+else
+	warn "/home/${USERNAME}/lsws/ does not exists."
 fi
+
+unset UID_TEMP USERNAME RED RESET
